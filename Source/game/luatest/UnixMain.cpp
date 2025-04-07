e4ede2deb3287cc0689c5e3b1c6b8ac049edcced
Fancy2209 <paulo.medeiros1302@gmail.com>
2025-04-07 18:27:37 +0000

    Make Source Tree closer to actual game

---
+++ Source/game/luatest/UnixMain.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    // Unreachable Code, should probably figure out if this is a bug
+    printf("Psychonauts Version: %c%d.%d\n", "P", 602, 0);
+    return 0;
+}
