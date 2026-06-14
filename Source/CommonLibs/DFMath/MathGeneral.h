1f37d5dfb72eb1b94f8e6ffa7a998fdef003fbd9
Leah Anderson <leah@chromebooks.lol>
2026-06-14 12:25:46 -0600

    yay! more functions decompiled (#6)
    * yay! more functions decomped!

---
+++ Source/CommonLibs/DFMath/MathGeneral.h
@@ -0,0 +1,9 @@
+#ifndef DF_MATHGENERAL_H
+#define DF_MATHGENERAL_H
+
+float Square(float value);
+bool Equal(float arg1, float arg2, float arg3);
+int ABSMAX(int num1, int num2);
+float Lerp(float num1, float num2, float num3);
+float SineRad(float num);
+#endif
